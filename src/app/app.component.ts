45039dba8f1c7b22ed221b4a54c4957f3e4e8543
Huong <huongnguyenak96@gmail.com>
2019-06-11 20:01:52 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: '#app-root',
+  // template: `
+  //   <h2>Hello Angular</h2>
+  //   <p>Welcome</p>
+  // `,
+  // styles: [
+  //   `
+  //   h2{
+  //     color:red
+  //   }
+  //   `,
+  //   `
+  //   p{
+  //     font-size: 30px
+  //   }
+  //   `
+  // ]
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
