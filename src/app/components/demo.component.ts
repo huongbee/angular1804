fd26a7359e9dd81e13424c974827993a4becb518
Huong <huongnguyenak96@gmail.com>
2019-06-13 20:42:49 +0700

    DemoComponent

---
+++ src/app/components/demo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-demo',
+    template: `
+        <h2>demo component</h2>
+    `
+})
+export class DemoComponent {
+
+}
+
+/**
+ * tạo component
+ * khai báo component app.module.ts
+ * gọi ra sử dụng
+ */
