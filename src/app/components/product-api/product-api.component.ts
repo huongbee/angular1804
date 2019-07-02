c904c396bc44e3bd4c42be84c0158a6435392109
Huong <huongnguyenak96@gmail.com>
2019-07-02 20:04:46 +0700

    form

--- src/app/components/product-api/product-api.component.ts
+++ src/app/components/product-api/product-api.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../types';
 import { RequestApiService } from '../../request-api.service';
 import { Store } from '@ngrx/store';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,11 +13,18 @@ import { Store } from '@ngrx/store';
 export class ProductApiComponent implements OnInit {
 
   products: Array<Product>;
+  formAdd: FormGroup;
 
   constructor(
     private requestService: RequestApiService,
-    private store: Store<Product[]>
+    private store: Store<Product[]>,
+    private fb: FormBuilder
   ) {
+    this.formAdd = this.fb.group({
+      name: ['', Validators.required],
+      price: ['0', Validators.required],
+    })
+
     this.requestService.getAllProduct()
     .then((data: Array<Product>)=>{
       this.store.dispatch({ type: 'INIT_PRODUCT', products: data })
